896842bfd40cff359b819073f23f5ab5b5a97397
mconn99 <mconn99@gmail.com>
2014-11-23 17:23:58 -0500

    Edited code for assignment and saved changes

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
+## MFC Comments:  This function sets & gets the value of the vector, then sets and gets the value of the mean
+
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 
 }
 
 
 ## Write a short comment describing this function
 
+##  MFC Comments:  This function calculates the mean of makeCacheMatrix, after checking if 
+## it has already been calculated and if so using the cached version
+
 cacheSolve <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
